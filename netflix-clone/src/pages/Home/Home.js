182ab99b4795d246c8297adcf899aa03e3a6c0ea
sifen <sifaman4@gmail.com>
2025-02-17 11:35:40 +0300

    row

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer/Footer'
 import Banner from '../../components/Banner/Banner'
+import RowList from '../../components/ROWS/RowList/RowList'
+
 
 function Home() {
   return (
     <>
         <Header />
         <Banner /> 
+        <RowList />
         <Footer />
     </>
   )
